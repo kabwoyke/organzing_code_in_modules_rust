e6f73b835f0bc7b7bf3e5ab1f3e8bae494540e75
kabwoyke <edwardkabwoy@gmail.com>
2026-05-14 21:29:20 +0300

    using the use keyword

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,18 @@
 mod DB {
 
-    mod read {
+    pub mod read {
         pub fn fetch_all(){}
         fn fetch_by_id(id:i32){}
 
     }
 
     
+   
 
     mod write {
+        use crate::DB::read::fetch_all;
         fn insert(data:String){
-            super::read::fetch_all();
+            fetch_all();
         }
         fn update(id:i32){}
         fn delete(){}
